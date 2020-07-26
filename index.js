04ea4191f6c1ee346d63182456582aeb2109c914
malis42 <mateuuszlis@gmail.com>
2020-07-26 12:10:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,83 @@
+import redditApi from "./redditApi";
+
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+// Form event listener
+searchForm.addEventListener('submit', ev => {
+    // Get search term string, sort-by condition and limit
+    const searchTerm = searchInput.value;
+    const searchSortBy = document.querySelector('input[name="sortby"]:checked').value;
+    const searchLimit = document.getElementById("limit").value;
+
+    // Validate input and clear it after error message pops up
+    if(searchTerm === ''){
+        showMessage('Please add a search term', 'alert-danger');
+    }
+    searchInput.value = "";
+
+    // Search through reddit
+    redditApi.searchPost(searchTerm, searchLimit, searchSortBy)
+        .then(results => {
+            let output = '<div class="card-columns">';
+
+            const defaultImage = 'http://www.eggmarketingpr.com/wp-content/uploads/2016/08/Reddit.jpg';
+
+            // Loop through posts
+            results.forEach(post =>{
+                // Check for image
+                const postImage = post.preview
+                    ? post.preview.images[0].source.url
+                    : defaultImage;
+
+                output += `
+                    <div class="card">
+                      <img src="${postImage}" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">${post.title}</h5>
+                        <p class="card-text">
+                            ${truncateText(post.selftext, 100)}
+                        </p>
+                        <a href="${post.url}" target="_blank" class="btn btn-primary">Read more</a>
+                        <hr>
+                        <span class="badge badge-secondary">Subreddit: ${post.subreddit}</span>
+                        <span class="badge badge-dark">Score: ${post.score}</span>
+                      </div>
+                    </div>
+                `;
+            });
+
+            output += '</div>';
+            document.getElementById('results').innerHTML = output;
+        });
+
+
+    ev.preventDefault();
+});
+
+// Show message function
+function showMessage(message, className) {
+    const messageDiv = document.createElement('div');
+
+    // Add classes to created div and add text to the message div
+    messageDiv.className = `alert ${className}`;
+    messageDiv.appendChild(document.createTextNode(message));
+
+    // Get parent container and search div
+    const searchParentContainer = document.getElementById('search-container');
+    const searchDiv = document.getElementById('search');
+
+    // Insert the message
+    searchParentContainer.insertBefore(messageDiv, searchDiv);
+
+    // Hide alert after some time
+    setTimeout(() => document.querySelector('.alert').remove(), 5000);
+}
+
+// Truncate text
+function truncateText(text, limit){
+    const shortenedText = text.indexOf(' ', limit);
+    if(shortenedText == -1) return text;
+    return text.substring(0, shortenedText);
+}
+
---
+++ redditApi.js
@@ -0,0 +1,9 @@
+export default {
+    searchPost: function(searchTerm, searchLimit, searchSortBy){
+        return fetch(`http://www.reddit.com/search.json?q=
+        ${searchTerm}&sort=${searchSortBy}&limit=${searchLimit}`)
+            .then(response => response.json())
+            .then(data => data.data.children.map(data => data.data))
+            .catch(error => console.log(error))
+    }
+};
